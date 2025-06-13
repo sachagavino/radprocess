367dae1ccc5c32c7b4a8bd6a4038fc8110bcb5a7
sachagavino <sacha.gavino@gmail.com>
2025-06-13 18:26:30 +0200

    pymsesrc

--- radprocess/pipeline/Convert.py
+++ radprocess/pipeline/Convert.py
@@ -14,7 +14,7 @@ class Convert:
     def __init__(self):
         self.root_directory = os.path.abspath(os.sep)
 
-    def update_pymsesrc(self, nb_grains=1, rho=True, vel=True, Br=False):
+    def update_pymsesrc(self, nb_grains=1, rho=True, vel=False, Bl=False, Br=False, P=False, Xi=False, phi=False, g=False):
 
         # Define the directory path
         pymses_directory = os.path.join(self.root_directory, ".pymses")
@@ -24,11 +24,40 @@ class Convert:
             os.makedirs(pymses_directory)
 
         # Define the file path in the pymses directory
-        file_path = os.path.join(pymses_directory, "pymsesrc")
+        rc_file = os.path.join(pymses_directory, "pymsesrc")
 
         # Create and write to the file
-        with open(file_path, "w") as file:
-            file.write("This is a file in the pymses directory.")
+        f = open(rc_file,"w")
+
+        f.write('{')
+        f.write('    "Version": 1,')
+        f.write('    "Multiprocessing max. nproc": 8,')
+        f.write('    "RAMSES":{')
+        f.write('        "ndimensions": 3,')
+        f.write('        "amr_field_descr": [')
+        if (rho==True):
+            f.write(f'            {"__type__": "scalar_field", "__file_type__": "hydro", "name": "rho", "ivar": 0},')
+        while i < nb_grains:
+            f.write(f'            {"__type__": "scalar_field", "__file_type__": "hydro", "name": "dustratio%d", "ivar": %d},' % (i+1, i+11))
+            i += 1
+        if (vel==True):
+            f.write(f'            {"__type__": "vector_field", "__file_type__": "hydro", "name": "vel", "ivars": [1, 2, 3]},')
+        if (Bl==True):
+            f.write(f'            {"__type__": "vector_field", "__file_type__": "hydro", "name": "Bl", "ivars": [4, 5, 6]},')
+        if (Br==True):
+            f.write(f'            {"__type__": "vector_field", "__file_type__": "hydro", "name": "Br", "ivars": [7, 8, 9]},')
+        if (P==True):
+            f.write(f'            {"__type__": "scalar_field", "__file_type__": "hydro", "name": "P", "ivar": 10},')
+        if (Xi==True):
+            f.write(f'            {"__type__": "multivalued_field", "__file_type__": "hydro", "name": "Xi", "ivar_first": 11, "nb_vars": 7},')
+        if (phi==True):
+            f.write(f'            {"__type__": "scalar_field", "__file_type__": "grav", "name": "phi", "ivar": 0},')
+        if (g==True):
+            f.write(f'            {"__type__": "vector_field", "__file_type__": "grav", "name": "g", "ivars": [1, 2, 3]}')
+        f.write('        ]')
+        f.write('    }')
+        f.write('}')
+        f.close()
 
 
     def to_radmc(self, nb_grains=1):
