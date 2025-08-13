20304cc785b6af3bac11b0dfd414d45a9d699e15
ilseunghan <99951713+ilseunghan@users.noreply.github.com>
2025-08-13 17:16:56 +0200

    Add files via upload

---
+++ radprocess/polaris/temp/wavelength_micron.py
@@ -0,0 +1,17 @@
+# Ilseung Han
+#   26.03.2025
+#   16.07.2025
+#   22.07.2025
+
+import os
+import numpy as np
+
+def wavelength_micron(wave = [0.27, 3000], nwave = 200):
+    wave = np.logspace(np.log10(wave[0]), np.log10(wave[1]), nwave)
+    os.system('rm -rf wavelength_micron.inp')
+    with open('wavelength_micron.inp', 'w') as f:
+        f.write('%d' % len(wave))
+        for w in wave:
+            f.write('\n%e' % w)
+
+wavelength_micron(wave = [0.27, 3000])
